e6cfaf58c90e8fd78da3237d936e8d8ada52e10e
Vivek Thakyal <vivek.thakyal@gmail.com>
2012-10-01 14:10:28 +0530

    fixed the message selector

--- bounce.js
+++ bounce.js
@@ -20,7 +20,7 @@ function init() {
     // start the animation loop
     setInterval(draw, g_interval);
   } else {
-    document.querySelector('div.message').style.display = 'static';
+    document.getElementById('message').style.display = 'static';
   }
 }
 
